dd9d73304810785f24a79c039221fefd4e079df9
Ryan Blakely <ryan@blakely.dev>
2022-03-02 21:57:25 -0500

    ui - updated project layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,11 +13,13 @@ const About = (props) => {
                 </header>
 
                 <main>
-                    <p>Hi 👋.</p>
-                    <p>
-                        I’m a software developer. When I learn something new, I like to write about it. This helps me
-                        remember how to do things in the future, and anyone else looking to learn.
-                    </p>
+                    <div>
+                        <p>Hi 👋.</p>
+                        <p>
+                            I’m a software developer. When I learn something new, I like to write about it. This helps
+                            me remember how to do things in the future, and anyone else looking to learn.
+                        </p>
+                    </div>
                 </main>
             </div>
         </React.Fragment>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -25,6 +25,28 @@ const Projects = (props) => {
 
                             <p>React, Python</p>
                         </div>
+                        <div className='project'>
+                            <img
+                                src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646268121/k2v3dbpcmsvm0gn9t2rk.png'
+                                alt=''
+                            />
+                            <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
+                                <h2>Watch Face</h2>
+                            </a>
+
+                            <p>Monkey C</p>
+                        </div>
+                        <div className='project'>
+                            <img
+                                src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646268121/k2v3dbpcmsvm0gn9t2rk.png'
+                                alt=''
+                            />
+                            <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
+                                <h2>VR Eye Training</h2>
+                            </a>
+
+                            <p>C</p>
+                        </div>
                     </div>
                 </main>
             </div>
