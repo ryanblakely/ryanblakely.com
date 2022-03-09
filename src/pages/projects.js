b4137e18c4b5f1ea81ad0e2942d188977ba1f864
Ryan Blakely <ryan@blakely.dev>
2022-03-09 14:52:57 -0500

    updated projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,15 +8,6 @@ const Projects = (props) => {
             <div className='page'>
                 <Navigation></Navigation>
                 <div className='projects'>
-                    {/* <section>
-                        <img
-                            src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646321851/szbx8ulg3ehhabdkh5yu.png'
-                            alt=''
-                        />
-                        <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
-                            Basix Watch Face
-                        </a>
-                    </section> */}
                     <section>
                         <img
                             src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646268121/k2v3dbpcmsvm0gn9t2rk.png'
@@ -26,15 +17,24 @@ const Projects = (props) => {
                             Magfinder
                         </a>
                     </section>
-                    {/* <section>
+                    <section>
+                        <img
+                            src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646855269/qhheww5ix4cywykgqgqo.svg'
+                            alt=''
+                        />
+                        <a href='https://nft-portfolio.netlify.app' target='_blank' rel='noreferrer'>
+                            NFT Portfolio
+                        </a>
+                    </section>
+                    <section>
                         <img
                             src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646277636/yzxaaewatswqrzhot4qk.png'
                             alt=''
                         />
-                        <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
+                        <a href='https://www.youtube.com/watch?v=9qJFNoTSZfY' target='_blank' rel='noreferrer'>
                             VR Eye Training
                         </a>
-                    </section> */}
+                    </section>
                 </div>
             </div>
         </React.Fragment>
