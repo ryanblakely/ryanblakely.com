60c1c9e1218abcaa15c919580bbfae870c61043e
Ryan Blakely <ryan@blakely.dev>
2022-03-09 17:08:17 -0500

    ui - added links to project logos

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,28 +9,34 @@ const Projects = (props) => {
                 <Navigation></Navigation>
                 <div className='projects'>
                     <section>
-                        <img
-                            src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646268121/k2v3dbpcmsvm0gn9t2rk.png'
-                            alt=''
-                        />
+                        <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
+                            <img
+                                src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646268121/k2v3dbpcmsvm0gn9t2rk.png'
+                                alt=''
+                            />
+                        </a>
                         <a href='https://www.magfinder.com' target='_blank' rel='noreferrer'>
                             Magfinder
                         </a>
                     </section>
                     <section>
-                        <img
-                            src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646855269/qhheww5ix4cywykgqgqo.svg'
-                            alt=''
-                        />
+                        <a href='https://nft-portfolio.netlify.app' target='_blank' rel='noreferrer'>
+                            <img
+                                src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646855269/qhheww5ix4cywykgqgqo.svg'
+                                alt=''
+                            />
+                        </a>
                         <a href='https://nft-portfolio.netlify.app' target='_blank' rel='noreferrer'>
                             NFT Wallet Tracker
                         </a>
                     </section>
                     <section>
-                        <img
-                            src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646862254/qdllerjnb9h8wok7ouju.svg'
-                            alt=''
-                        />
+                        <a href='https://www.youtube.com/watch?v=9qJFNoTSZfY' target='_blank' rel='noreferrer'>
+                            <img
+                                src='https://res.cloudinary.com/dccqw6mij/image/upload/v1646862254/qdllerjnb9h8wok7ouju.svg'
+                                alt=''
+                            />
+                        </a>
                         <a href='https://www.youtube.com/watch?v=9qJFNoTSZfY' target='_blank' rel='noreferrer'>
                             VR Eye Exercises
                         </a>
